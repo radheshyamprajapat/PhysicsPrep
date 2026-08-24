9156fddfe4ffbc8f4617fd5114ea851b7be148ba
RS <101662241+radheshyamprajapat@users.noreply.github.com>
2026-08-24 11:09:44 +0530

    Create solutions.js

---
+++ data/solutions.js
@@ -0,0 +1,63 @@
+{
+    subject: "nuclear",
+    name: "Nuclear Physics",
+    icon: "☢️",
+
+    topics: [
+
+        {
+            id: "basic-nuclear-ground-state-properties",
+            name: "Basic Nuclear Ground State Properties",
+            pdf: "solutions/nuclear/basic-nuclear-ground-state-properties.pdf"
+        },
+
+        {
+            id: "properties-of-nuclear-force",
+            name: "Properties Of Nuclear Force",
+            pdf: "solutions/nuclear/properties-of-nuclear-force.pdf"
+        },
+
+        {
+            id: "nuclear-model",
+            name: "Nuclear Model",
+            pdf: "solutions/nuclear/nuclear-model.pdf"
+        },
+
+        {
+            id: "deuteron-nuclei",
+            name: "Deuteron Nuclei",
+            pdf: "solutions/nuclear/deuteron-nuclei.pdf"
+        },
+
+        {
+            id: "nuclear-radioactive-decays",
+            name: "Nuclear Radioactive Decays",
+            pdf: "solutions/nuclear/nuclear-radioactive-decays.pdf"
+        },
+
+        {
+            id: "radioactivity",
+            name: "Radioactivity",
+            pdf: "solutions/nuclear/radioactivity.pdf"
+        },
+
+        {
+            id: "nuclear-reaction",
+            name: "Nuclear Reaction",
+            pdf: "solutions/nuclear/nuclear-reaction.pdf"
+        },
+
+        {
+            id: "nuclear-detector",
+            name: "Nuclear Detector",
+            pdf: "solutions/nuclear/nuclear-detector.pdf"
+        },
+
+        {
+            id: "particle-physics",
+            name: "Particle Physics",
+            pdf: "solutions/nuclear/particle-physics.pdf"
+        }
+
+    ]
+}
